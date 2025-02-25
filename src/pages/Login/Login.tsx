0b33122c049fd27cdf02ba9a113adf7bca26eddc
Albert Stive <stiven23ospina@gmail.com>
2025-02-24 22:27:43 -0500

    avance

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
 
-import LogoArgus from '@assets/img/Argus-Logo.svg'
+import LogoArgus from "@assets/img/Argus-Logo.svg";
 
-export type LoginProps = {
+export type LoginProps = {};
 
-}
-
-const Login: React.FC<LoginProps>  = ({}) => {
-	return (
-		<section className='login'>
- 			<article>
-				<form action="">
-					<img src={LogoArgus} alt="" width={200} />
-					<label htmlFor="email">Email</label>
-					<input type="text" id="email" />
-					<label htmlFor="password">Contraseña</label>
-					<input type="password" name="" id="password" />
-				</form>
-			</article>
-			<article className='login--banner'>
-				<img src="" alt="" />
-			</article>
- 		</section>
-	);
+const Login: React.FC<LoginProps> = ({}) => {
+  return (
+    <section className="login">
+      <article className="login__form">
+        <form action="">
+          <div className="form__header">
+            <img src={LogoArgus} alt="" />
+          </div>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" />
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" name="" id="password" />
+          <button type="button">Ingresar</button>
+        </form>
+      </article>
+      <article className="login--banner">
+        <img src="" alt="" />
+      </article>
+    </section>
+  );
 };
 
 export default Login;
